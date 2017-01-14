49430ff86937a26dc4b55abdadc91617617fad49
Ian R. Connelly <ian@cyberdust.com>
2017-01-14 10:35:08 -0800

    Adding testing screen UI.

---
+++ MomentUS/MomentUS/TestViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  MomentUS
+//
+//  Created by Ian Connelly on 1/14/17.
+//  Copyright © 2017 Ian Connelly. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    @IBOutlet weak var _statusLabel: UILabel!
+    @IBOutlet weak var _testButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _updateButtonUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func _updateButtonUI() {
+        _testButton.setTitle("Listen", for: .normal)
+        _testButton.layer.cornerRadius = 5
+        _testButton.contentEdgeInsets = UIEdgeInsetsMake(5, 30, 5, 30)
+    }
+    
+    private func _updateLabelText(newText: String) {
+        _statusLabel.text = newText
+    }
+
+    @IBAction func testButtonAction() {
+        _updateLabelText(newText: "Im listening!")
+    }
+
+}
+
