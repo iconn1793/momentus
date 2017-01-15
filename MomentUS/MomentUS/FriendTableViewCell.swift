b8677410b33ed52f4dc705bd7ce95ca67e7c5208
Eric Connelly <eric.connelly08@gmail.com>
2017-01-15 18:09:52 -0500

    Setup for FriendTableCellView

---
+++ MomentUS/MomentUS/FriendTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FriendTableViewCell.swift
+//  MomentUS
+//
+//  Created by Eric Connelly on 1/15/17.
+//  Copyright © 2017 Ian Connelly. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
+
